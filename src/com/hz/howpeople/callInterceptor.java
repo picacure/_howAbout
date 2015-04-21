5cda4ea5dcd72a845983fd6a5cdd3b9d76891369
jiangcheng.wxd <jiangcheng.wxd@taobao.com>
2015-04-21 16:56:30 +0800

    all today's work

--- src/com/hz/howpeople/callInterceptor.java
+++ src/com/hz/howpeople/callInterceptor.java
@@ -8,36 +8,74 @@ import android.telephony.TelephonyManager;
 import android.view.Gravity;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.os.Handler;
+
+
 /**
  * Created by admin on 15-4-20.
  */
 public class callInterceptor extends BroadcastReceiver {
+
+    Timer mTimer = new Timer();
+
+    /**
+     * 此方法利用TimerTask在Toast显示一秒后再显示一次。
+     */
+    private void execToast(final Toast toast) {
+
+//        timer.schedule(new TimerTask() {
+//
+//            @Override
+//            public void run() {
+//                //调主线程方法，否则可能会显示不出来。
+//                initToast(toast);
+//            }
+//
+//        }, 1000);
+
+        mTimer.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                //调主线程方法，否则可能会显示不出来。
+                initToast(toast);
+            }
+
+        }, 0, 1000);
+    }
+
+    private void initToast(Toast toast) {
+        toast.show();
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
         String msg = "友评提示：（";
 
-        if (TelephonyManager.EXTRA_STATE_RINGING.equals(state) || TelephonyManager.EXTRA_STATE_OFFHOOK.equals(state)) {
+        if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
             String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
             msg += incomingNumber + "):友评得分：8分";
 
 
-            final Toast tt = Toast.makeText(context, msg, Toast.LENGTH_LONG);
-            tt.setGravity(Gravity.TOP | Gravity.CENTER, 0, 300);
-            tt.show();
+            final Toast mToast = Toast.makeText(context, msg, Toast.LENGTH_LONG);
+            mToast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 300);
+            mToast.show();
+            execToast(mToast);
 
-            new CountDownTimer(11000, 1000) {
-                public void onTick(long millisUntilFinished) {
-                    tt.show();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mTimer.cancel();
                 }
-
-                public void onFinish() {
-                    tt.show();
-                }
-
-            }.start();
+            }, 5000);
         }
+
+
 
     }
 }
