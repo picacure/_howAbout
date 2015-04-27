557c246736019242a4ef4bbcafbf924626659172
jiangcheng.wxd <jiangcheng.wxd@taobao.com>
2015-04-27 12:05:07 +0800

    添加对播出电话支持

--- src/com/hz/howpeople/phoneService.java
+++ src/com/hz/howpeople/phoneService.java
@@ -32,6 +32,7 @@ public class phoneService extends Service {
 
         IntentFilter filter = new IntentFilter();
         filter.addAction("android.intent.action.PHONE_STATE");
+        filter.addAction("android.intent.action.NEW_OUTGOING_CALL");
 
 
         if (phoneStateReceiver == null) {
@@ -43,21 +44,58 @@ public class phoneService extends Service {
                     String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
                     String msg = "友评提示：（";
 
-                    if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-                        String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                        msg += incomingNumber + "):友评得分：8分";
 
-                        lastIncoming = incomingNumber;
+                    final String oldNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+                    //电话打出.
+                    if(oldNumber != null){
+                        msg += oldNumber + "):友评得分：8分";
 
                         final Toast mToast = Toast.makeText(context, msg, Toast.LENGTH_LONG);
                         mToast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 300);
                         mToast.show();
 
-                    } else {
-                        if (lastIncoming != "") {
-                            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
 
-                            } else {
+                        //通知.
+                        nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+                        //设置不同Id，同一个App可以显示多次.
+                        nm.cancel(1);
+
+
+                        //防止不同手机拨号后清除和设置被OS优化 而不显示 notification.
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+
+                            @Override
+                            public void run() {
+
+                                Calendar calendar = Calendar.getInstance();
+                                Notification notification = new Notification(R.drawable.ic, "友评：点此匿名评价益之", calendar.getTimeInMillis());
+
+                                PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                                        new Intent(context, MyActivity.class), 0);
+
+                                notification.setLatestEventInfo(context, "友评：点此匿名评价益之", lastIncoming, contentIntent);
+                                notification.flags = Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL;
+
+                                nm.notify(1, notification);
+                            }
+                        }, 1000);
+                    }
+                    // 打进
+                    else {
+                        if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                            String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                            msg += incomingNumber + "):友评得分：8分";
+
+                            lastIncoming = incomingNumber;
+
+                            final Toast mToast = Toast.makeText(context, msg, Toast.LENGTH_LONG);
+                            mToast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 300);
+                            mToast.show();
+
+                        } else {
+                            if (lastIncoming != "") {
                                 if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
 
                                     nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
